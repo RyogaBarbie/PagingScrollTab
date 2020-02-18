0076f629c5af2da58e35db6d726ea1c2e91a5805
ryogabarbie <ryoga1226@gmail.com>
2020-02-18 12:31:34 +0900

    create project

---
+++ PagingScrollTab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PagingScrollTab
+//
+//  Created by YamamuraRyoga on 2020/02/18.
+//  Copyright © 2020 y.ryoga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
